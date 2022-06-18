003c26302213591c3b92d5d0286e5089f10fd44c
deivid.fabiano <deivid.fabiano@bv.com.br>
2022-06-18 16:42:51 -0300

    Configurar e validar a conexão do projeto backend a database

--- bin/base_backend.dart
+++ bin/base_backend.dart
@@ -1,8 +1,23 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:mysql1/mysql1.dart';
 
-void main(List<String> arguments) {
-  serve(
+void main(List<String> arguments) async {
+  final conn = await MySqlConnection.connect(
+    ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      db: 'delivery',
+      password: 'rott',
+    ),
+  );
+
+  await conn
+      .query("insert into tb_permissoes(nome, status) values ('ADMIN', 'A')");
+  print(await conn.query('select * from tb_permissoes'));
+
+  await serve(
     (Request req) => Response(200,
         body: 'Olá Mundo', headers: {'content-type': 'application/json'}),
     'localhost',
